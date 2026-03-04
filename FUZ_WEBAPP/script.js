257b05ce474304cfc9c39410b7cd86838417b9eb
Jacopo Fusè <fuse.jacopo@itiscannizzaro.edu.it>
2026-03-04 12:16:51 +0100

    Create script.js

---
+++ FUZ_WEBAPP/script.js
@@ -0,0 +1,8 @@
+// Mobile Menu Toggle
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  mobileMenu.classList.toggle('open');
+});
